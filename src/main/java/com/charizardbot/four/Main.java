3e77ca03a7ced8b9c5c8c9328a000f56ea523130
jg99 <jamesgryffindor99@gmail.com>
2019-12-07 12:07:37 -0800

    version 4.0.2-1, add changelog file

--- src/main/java/com/charizardbot/four/Main.java
+++ src/main/java/com/charizardbot/four/Main.java
@@ -42,7 +42,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 public class Main {
-	public static final String VERSION = "4.0.2";
+	public static final String VERSION = "4.0.2-1";
 	public static String filterDB = "";
 	public static File chatFilter;
     public static String filterFile = "chatfilter.txt";
