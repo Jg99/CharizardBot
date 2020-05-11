c3800a561430794b433e11060537cfda9d91a8e5
jg99 <jamesgryffindor99@gmail.com>
2020-05-10 20:10:42 -0700

    test

--- src/main/java/com/charizardbot/four/KITableCrawler.java
+++ src/main/java/com/charizardbot/four/KITableCrawler.java
@@ -14,11 +14,9 @@ public class KITableCrawler {
 		contents = "";
 		table = "";
 		String userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/74.0.3729.157 Safari/537.36";
-        @SuppressWarnings("deprecation")
 		org.jsoup.nodes.Document doc = Jsoup.connect("http://www.wizard101.com/pvp/schedule")
         		.userAgent(userAgent)
         		.timeout(20000)
-        		.validateTLSCertificates(false)
         		.get();
         org.jsoup.select.Elements rows = doc.select("tr");
         for(org.jsoup.nodes.Element row :rows)
