0a181a62cba0eee352b6a7e935f4189cb87dcbc9
jg99 <jamesgryffindor99@gmail.com>
2022-10-24 10:48:48 -0600

    update dependencies

--- src/main/java/com/charizardbot/main/ReconnectListener.java
+++ src/main/java/com/charizardbot/main/ReconnectListener.java
@@ -1,12 +1,12 @@
 package com.charizardbot.main;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.events.ReconnectedEvent;
+import net.dv8tion.jda.api.events.session.SessionRecreateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class ReconnectListener extends ListenerAdapter {
     /**
      * resets the game status when CharizardBot reconnects.
      */
-public void onReconnect (ReconnectedEvent event) {
+public void onReconnect (SessionRecreateEvent event) {
     String activity = "";
     if (Main.config.getProperty("gamestatus") == null || Main.config.getProperty("gamestatus").equals("")) {
         activity = "Battling Team Rocket";
